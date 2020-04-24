8421d4a3d0e37966ec6b7e106061d7c6a2e3e02f
midza04 <midza04@users.noreply.github.com>
2020-04-24 20:49:18 +0200

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace AndroidSigner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUnsignedAPK = new System.Windows.Forms.TextBox();
+            this.txtSignedAPK = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtJarsigner = new System.Windows.Forms.TextBox();
+            this.txtZipAligner = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUnsignedAPK
+            // 
+            resources.ApplyResources(this.txtUnsignedAPK, "txtUnsignedAPK");
+            this.txtUnsignedAPK.Name = "txtUnsignedAPK";
+            this.txtUnsignedAPK.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtSignedAPK
+            // 
+            resources.ApplyResources(this.txtSignedAPK, "txtSignedAPK");
+            this.txtSignedAPK.Name = "txtSignedAPK";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtJarsigner
+            // 
+            resources.ApplyResources(this.txtJarsigner, "txtJarsigner");
+            this.txtJarsigner.Name = "txtJarsigner";
+            this.txtJarsigner.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtZipAligner
+            // 
+            resources.ApplyResources(this.txtZipAligner, "txtZipAligner");
+            this.txtZipAligner.Name = "txtZipAligner";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            resources.ApplyResources(this.textBox5, "textBox5");
+            this.textBox5.Name = "textBox5";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtZipAligner);
+            this.Controls.Add(this.txtJarsigner);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSignedAPK);
+            this.Controls.Add(this.txtUnsignedAPK);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUnsignedAPK;
+        private System.Windows.Forms.TextBox txtSignedAPK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtJarsigner;
+        private System.Windows.Forms.TextBox txtZipAligner;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Web;
+
+namespace AndroidSigner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // progressBar2.Visible = false;
+            txtJarsigner.Text = "C:/Key";
+            txtZipAligner.Text = "C:/Key";
+            txtZipAligner.ReadOnly = true;
+            txtJarsigner.ReadOnly = true;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //progressBar2.Visible = true;
+          
+            button1.Text = "Please Wait...";
+            button1.Enabled = false;
+            if (txtUnsignedAPK.Text.Length < 4)
+            {
+                MessageBox.Show("Enter path to access the APK file you want to sign");
+                return;
+            }
+
+            if (txtSignedAPK.Text.Length < 4)
+            {
+                MessageBox.Show("Enter the Output name of the signed APK");
+                return;
+            }
+            //string path = HttpContext.Current.Server.MapPath("Packages");
+            string argument = @"/C jarsigner -verbose -keypass militant  -sigalg SHA1withRSA -digestalg SHA1 -keystore C:\Key\my-release-key.keystore" + " " + txtUnsignedAPK.Text + " " + "alias_name -storepass militant";
+            Process p = new Process
+          
+            {
+                StartInfo =
+                    {
+                        WorkingDirectory = @"C:\Program Files\Java\jdk1.8.0_231\bin",
+                        FileName =  "cmd.exe",
+                        //Arguments = @"/C mkdir C:\Key\asd" ,
+                        Arguments =  argument,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = false,
+                        RedirectStandardInput = false,
+                        CreateNoWindow = true,
+                       
+            //WorkingDirectory = @"C:\Program Files\wkhtmltopdf\bin"
+        }
+            };
+
+            try
+            {
+               // progressBar2.Value = 50;
+                p.Start();
+                
+                textBox5.Text = p.StandardOutput.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //textBox5.Text = ex.Message;
+                //Log(ex.Message, "Fee Generated");
+            }
+
+            // I think it's timimg out here for shoo
+            // Emmanuel Picked this up 
+            // This should be put conditionally if records are more then increase the wait period
+            p.WaitForExit(999999999);
+
+            //dataLayer.LogStep("p WaitForExit", "Generate PDF finished waiting");
+
+            //p.WaitForExit(); 
+
+            int returnCode = p.ExitCode;
+            p.Close();
+
+            
+
+            //dataLayer.LogStep("PDF Generator", "PDF successfully generated");
+
+            if (returnCode != 0 && returnCode != 2)
+            {
+                //throw new Exception();
+            }
+
+           
+
+            p.Dispose();
+           // progressBar2.Visible = false;
+
+            processZipalign();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void processZipalign()
+        {
+
+            Process proc = new Process
+            {
+                StartInfo =
+                    {
+                        WorkingDirectory = @"C:\Key",
+                        FileName =  "cmd.exe",
+                        //Arguments = @"/C mkdir C:\Key\asd" ,
+                        Arguments = @"/C zipalign -v 4 "+" "+txtUnsignedAPK.Text+" "+" "+txtSignedAPK.Text+".apk" ,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = false,
+                        RedirectStandardInput = false,
+                        CreateNoWindow = true,
+                       
+            //WorkingDirectory = @"C:\Program Files\wkhtmltopdf\bin"
+             }
+            };
+
+            try
+            {
+               // progressBar2.Value = 50;
+                proc.Start();
+
+                textBox5.Text = proc.StandardOutput.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //textBox5.Text = ex.Message;
+                //Log(ex.Message, "Fee Generated");
+            }
+
+            // I think it's timimg out here for shoo
+            // Emmanuel Picked this up 
+            // This should be put conditionally if records are more then increase the wait period
+            proc.WaitForExit(999999999);
+
+            //dataLayer.LogStep("p WaitForExit", "Generate PDF finished waiting");
+
+            //p.WaitForExit(); 
+
+            int returnCode = proc.ExitCode;
+            proc.Close();
+
+
+
+            //dataLayer.LogStep("PDF Generator", "PDF successfully generated");
+
+            if (returnCode != 0 && returnCode != 2)
+            {
+                //throw new Exception();
+            }
+
+
+
+            proc.Dispose();
+           // progressBar2.Visible = false;
+            button1.Enabled = true;
+            button1.Text = "Generate Another Signed APK";
+
+            MessageBox.Show("APK was signed Successfully!!!!");
+            Process.Start(@"C:\Key");
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
